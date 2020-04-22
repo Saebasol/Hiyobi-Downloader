15ca8e5af3e7c9f37a5b31f2867663e87789cb34
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-04-23 08:42:10 +0900

    Closed #1
    Can setting Connection limit

--- downloder.py
+++ downloder.py
@@ -21,16 +21,19 @@ async def getimg(num):
         listimg = [d['name'] for d in json]
         return listimg
 
-async def download(s: str,num):
-    async with aiohttp.ClientSession() as resp:
+async def download(s: str, num, limit):
+    conn = aiohttp.TCPConnector(limit=int(limit))
+    async with aiohttp.ClientSession(connector=conn) as resp:
         async with resp.get(f'https://cdn.hiyobi.me/data/{num}/{s}') as res:
             async with aiofiles.open(f'{num}/{s}', mode='wb') as f:
                 await f.write(await res.read())
+                
 
 if __name__ == '__main__':
     num = input("번호를 입력해주세요\n")
+    limit = input("제한할 연결수를 입력해주세요(2~3추천)\n")
     loop = asyncio.get_event_loop()
     createFolder(f'./{num}')
     lists = loop.run_until_complete(getimg(num=num))
-    tasks = [download(x, num=num) for x in lists]
+    tasks = [download(x, num=num, limit=limit) for x in lists]
     loop.run_until_complete(asyncio.wait(tasks))
