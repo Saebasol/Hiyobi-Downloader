62f0a97a1da76fbcc880eafadc7a5c2fa1eb0ecc
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-04-23 22:41:21 +0900

    Feat: No case sensitivity required

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import downloder
 import search
 
 loop = asyncio.get_event_loop()
+
 check = input("다운은 1번 검색은 2번을 입력해주세요\n")
 if check == str(1):
     num = input("번호를 입력해주세요\n")
@@ -11,7 +12,6 @@ if check == str(1):
     tasks = [downloder.download(x, num=num) for x in lists]
     loop.run_until_complete(asyncio.wait(tasks))
 
-
 if check == str(2):
     check = input("리스트검색은 1번, 제목,태그검색은 2번을 입력해주세요\n")
 
@@ -19,7 +19,7 @@ if check == str(2):
         num = input("리스트검색입니다. 몇번쨰 페이지를 검색할까요?\n")
         idlist = loop.run_until_complete(search.hiyobilist(num=num))
         check = input("다운로드할까요? Y/N\n")
-        if check == str('Y' or 'y'):
+        if check == str('Y') or str('y'):
             num = input("리스트중 골라주세요\n")
             hiyoid = idlist[int(num) - 1]
             downloder.createFolder(f'./{hiyoid}')
@@ -31,7 +31,7 @@ if check == str(2):
         para = input("제목,태그검색입니다. 어떤걸 검색할까요?\n")
         idlist = loop.run_until_complete(search.hiyobisearch(para=para))
         check = input("다운로드할까요? Y/N\n")
-        if check == str('Y' or 'y'):
+        if check == str('Y') or str('y'):
             num = input("리스트중 골라주세요\n")
             hiyoid = idlist[int(num) - 1]
             downloder.createFolder(f'./{hiyoid}')
