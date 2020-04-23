e6c2b8ab9f6d106d240e94c7f755927dfba11066
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-04-23 09:56:37 +0900

    Changed from connection limit to asynchronous limit

--- downloder.py
+++ downloder.py
@@ -3,7 +3,10 @@ import aiohttp
 import aiofiles
 import json
 import os
- 
+
+semnum = input("한번에 다운받을수를 입력해주세요(보통 1~5추천,cg집같은경우는 알아서 맞추세요.)\n")
+sem = asyncio.Semaphore(int(semnum))
+
 def createFolder(directory):
     try:
         if not os.path.exists(directory):
@@ -21,19 +24,18 @@ async def getimg(num):
         listimg = [d['name'] for d in json]
         return listimg
 
-async def download(s: str, num, limit):
-    conn = aiohttp.TCPConnector(limit=int(limit))
-    async with aiohttp.ClientSession(connector=conn) as resp:
-        async with resp.get(f'https://cdn.hiyobi.me/data/{num}/{s}') as res:
-            async with aiofiles.open(f'{num}/{s}', mode='wb') as f:
-                await f.write(await res.read())
+async def download(s: str, num):
+    async with sem:
+        async with aiohttp.ClientSession() as resp:
+            async with resp.get(f'https://cdn.hiyobi.me/data/{num}/{s}') as res:
+                async with aiofiles.open(f'{num}/{s}', mode='wb') as f:
+                    await f.write(await res.read())
                 
 
 if __name__ == '__main__':
     num = input("번호를 입력해주세요\n")
-    limit = input("제한할 연결수를 입력해주세요(0입력할시 제한없음,보통2~3추천)\n")
     loop = asyncio.get_event_loop()
     createFolder(f'./{num}')
     lists = loop.run_until_complete(getimg(num=num))
-    tasks = [download(x, num=num, limit=limit) for x in lists]
+    tasks = [download(x, num=num) for x in lists]
     loop.run_until_complete(asyncio.wait(tasks))
