90204c6eff54c5ba1dc17464395a6a62b84f73ec
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-04-24 11:59:55 +0900

    Fixed: Edit dupe code

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import search
 
 loop = asyncio.get_event_loop()
 
+def chkdown():
+    check = input("다운로드할까요? Y/N\n")
+    if check == str('Y') or check == str('y'):
+        num = input("리스트중 골라주세요\n")
+        hiyoid = idlist[int(num) - 1]
+        downloder.createFolder(f'./{hiyoid}')
+        lists = loop.run_until_complete(downloder.getimg(num=hiyoid))
+        tasks = [downloder.download(x, num=hiyoid) for x in lists]
+        loop.run_until_complete(asyncio.wait(tasks))
+
 check = input("다운은 1번 검색은 2번을 입력해주세요\n")
 
 if check == str(1):
@@ -19,23 +29,9 @@ if check == str(2):
     if check == str(1):
         num = input("리스트검색입니다. 몇번쨰 페이지를 검색할까요?\n")
         idlist = loop.run_until_complete(search.hiyobilist(num=num))
-        check = input("다운로드할까요? Y/N\n")
-        if check == str('Y') or check == str('y'):
-            num = input("리스트중 골라주세요\n")
-            hiyoid = idlist[int(num) - 1]
-            downloder.createFolder(f'./{hiyoid}')
-            lists = loop.run_until_complete(downloder.getimg(num=hiyoid))
-            tasks = [downloder.download(x, num=hiyoid) for x in lists]
-            loop.run_until_complete(asyncio.wait(tasks))
+        chkdown()
 
     if check == str(2):
         para = input("제목,태그검색입니다. 어떤걸 검색할까요?\n")
         idlist = loop.run_until_complete(search.hiyobisearch(para=para))
-        check = input("다운로드할까요? Y/N\n")
-        if check == str('Y') or check == str('y'):
-            num = input("리스트중 골라주세요\n")
-            hiyoid = idlist[int(num) - 1]
-            downloder.createFolder(f'./{hiyoid}')
-            lists = loop.run_until_complete(downloder.getimg(num=hiyoid))
-            tasks = [downloder.download(x, num=hiyoid) for x in lists]
-            loop.run_until_complete(asyncio.wait(tasks))
+        chkdown()
